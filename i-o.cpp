2a19e134a505b884d99bc54cd62ec02e4b6225b8
Harshwardhan Vishwakarma <hvishwakarma1729@gmail.com>
2025-01-07 08:18:20 +0530

    input output in cpp

---
+++ i-o.cpp
@@ -0,0 +1,34 @@
+// input output in cpp
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a, b;
+
+    cout << "Enter a number: ";
+
+    cin >> a;
+
+    cout << "Enter another number: ";
+
+    cin >> b;
+
+    cout << "The sum of the numbers is: " << a + b << endl;
+
+
+    cout << "asking together now !!!" << endl;
+    cout << endl;
+
+
+cout << "Enter value of a and b: ";
+
+cin >> a >> b;
+
+cout << "The sum of the numbers is: " << a + b << endl;
+
+
+    return 0;
+}
