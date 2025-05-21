1dba7dd226b007ec5c1a4f5c5bd30d657fd64aae
Harshwardhan Vishwakarma <hvishwakarma1729@gmail.com>
2025-05-21 11:41:04 +0530

    pair sum with brute forace and optimized method

---
+++ DSA/Arrays/pairsum.cpp
@@ -0,0 +1,63 @@
+// pair sum
+
+
+#include <iostream>
+
+#include <vector>
+
+
+using namespace std; 
+
+
+
+
+// using brute force 
+
+
+
+vector<int> PairSum( vector<int> nums , int target ) {
+
+    vector<int> ans ;
+
+    int n = nums.size() ;
+
+
+    for ( int i =0 ; i < n ; i++ ) {
+
+        for ( int j = i+1 ; j < n ; j++ ) {
+
+            if ( nums[i]+ nums[j]== target) {
+                ans.push_back(i) ;
+                ans.push_back(j) ;
+                return ans ;
+            }
+
+
+        }
+    }
+
+
+
+    return ans; 
+
+
+
+}
+
+
+
+
+
+int main () {
+
+    vector <int> nums = { 2,7,11,15} ;
+    
+    int target = 13 ;
+
+    vector <int > ans = PairSum(nums,target);
+    
+    cout << ans[0] << "," << ans[1] << endl ;
+
+
+    return 0; 
+}
