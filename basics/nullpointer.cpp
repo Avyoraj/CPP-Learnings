a36ea1a9fe24d9a69db02c4d6c7b1a621a32ea6a
Harshwardhan Vishwakarma <hvishwakarma1729@gmail.com>
2025-05-25 12:16:29 +0530

    Create nullpointer.cpp

---
+++ basics/nullpointer.cpp
@@ -0,0 +1,22 @@
+// null pointers 
+
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    
+    int*  ptr = NULL ;
+
+
+
+    cout << ptr << endl; // 0 
+   
+    
+    
+    
+    return 0;
+}
+
