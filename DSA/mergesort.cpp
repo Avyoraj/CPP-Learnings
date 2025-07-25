73fe48f8c7262dc15a87fc9ac2fd4d70baf46a41
Harshwardhan Vishwakarma <hvishwakarma1729@gmail.com>
2025-07-26 00:20:25 +0530

    Update mergesort.cpp

--- DSA/mergesort.cpp
+++ DSA/mergesort.cpp
@@ -4,77 +4,86 @@
 
 using namespace std;
 
-// Merges two subarrays of vec.
-// First subarray is vec[left..mid]
-// Second subarray is vec[mid+1..right]
-void merge(vector<int>& vec, int left, int mid, int right) {
-    int i, j, k;
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
+// Merges arrage using recursive merge sort
 
-    // Create temporary vectors
-    vector<int> leftVec(n1), rightVec(n2);
+void merge(vector<int> &arr, int st, int mid , int end ) {   // this function merges two halves of the array
 
-    // Copy data to temporary vectors
-    for (i = 0; i < n1; i++)
-        leftVec[i] = vec[left + i];
-    for (j = 0; j < n2; j++)
-        rightVec[j] = vec[mid + 1 + j];
 
-    // Merge the temporary vectors back into vec[left..right]
-    i = 0;
-    j = 0;
-    k = left;
-    while (i < n1 && j < n2) {
-        if (leftVec[i] <= rightVec[j]) {
-            vec[k] = leftVec[i];
+    vector<int> temp;
+
+    int i = st , j = mid+1;
+
+    while(i <= mid && j <= end ) {
+        if ( arr[i] <= arr[j]) {   // for desending just use >=
+            temp.push_back(arr[i]) ;
             i++;
-        } else {
-            vec[k] = rightVec[j];
+        }
+        else {
+            temp.push_back(arr[j]);
             j++;
         }
-        k++;
     }
 
-    // Copy the remaining elements of leftVec[], if any
-    while (i < n1) {
-        vec[k] = leftVec[i];
+
+
+    while (i <= mid)
+    {
+         temp.push_back(arr[i]) ;
         i++;
-        k++;
     }
 
-    // Copy the remaining elements of rightVec[], if any
-    while (j < n2) {
-        vec[k] = rightVec[j];
-        j++;
-        k++;
+
+    while (j <= end)
+    {
+      temp.push_back(arr[j]);
+      j++;
+    }
+
+
+    for(int idx = 0 ; idx <temp.size(); idx++) {
+        arr[idx+st] = temp[idx];
+    }
+    
+    
+
+}
+
+
+
+
+
+void mergeSort (vector<int> &arr, int st ,int end ) { // this function sorts the array using merge sort algorithm
+
+
+    if(st<end) {
+        int mid = st + ( end - st)/2 ;
+
+
+        mergeSort ( arr , st , mid ) ; // left half
+        mergeSort ( arr , mid+1 , end) ; // right side
+
+
+        merge(arr,st,mid,end);
+        
+    
     }
 }
 
-// The subarray to be sorted is in the index range [left..right]
-void mergeSort(vector<int>& vec, int left, int right) {
-    if (left < right) {
-      
-        // Calculate the midpoint
-        int mid = left + (right - left) / 2;
 
-        // Sort first and second halves
-        mergeSort(vec, left, mid);
-        mergeSort(vec, mid + 1, right);
 
-        // Merge the sorted halves
-        merge(vec, left, mid, right);
+
+
+int main(int argc, char const *argv[])
+{
+    vector <int> arr = {12,31,35,8,32,17};
+
+    mergeSort(arr,0,arr.size()-1); // calling merge sort function
+    
+    for(int val : arr) {
+        cout << val << " "  ;
     }
-}
 
-int main() {
-    vector<int> vec = {12, 11, 13, 5, 6, 7};
-    int n = vec.size();
+    cout << endl;
 
-    // Sorting vec using mergesort
-    mergeSort(vec, 0, n - 1);
-
-    for (auto i: vec)
-        cout << i << " ";
     return 0;
 }
